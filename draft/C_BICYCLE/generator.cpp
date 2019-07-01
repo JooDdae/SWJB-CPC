1c3f753e0982388ee823cb4ef34f63c3c9867a1e
JooDdae <kyo20111@naver.com>
2019-07-01 20:32:16 +0900

    update generator

--- draft/C_BICYCLE/generator.cpp
+++ draft/C_BICYCLE/generator.cpp
@@ -8,18 +8,20 @@ int main(int argc, char* argv[]){
 	int a = atoi(argv[1]);
 	int b = atoi(argv[2]);
 	int c = atoi(argv[3]);
-    vector<int> v;
+    vector<int> v,num;
     for(int i=1;i<=360;i++) if(360%i == 0) v.push_back(i);
 	int N = rnd.next(0, a-1);
 	printf("%d\n", v[N]);
+	for(int i=0;i<=v[N];i++) num.push_back(i);
+	shuffle(num.begin()+1,num.end());
 	for(int i=1;i<=v[N];i++){
-        printf("%d",i);
+        printf("%d",num[i]);
         if(i < v[N]) printf(" ");
 	}
 	printf("\n");
 	int K = rnd.next(0, b);
 	for(int i=1;i<=v[N];i++){
-        printf("%d",(i-1+K)%v[N]+1);
+        printf("%d",num[(i-1+K)%v[N]+1]);
         if(i < v[N]) printf(" ");
 	}
     printf("\n");
