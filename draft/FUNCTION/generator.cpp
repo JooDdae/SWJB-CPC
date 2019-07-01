946001e44890bc9babd9b198b560236053619d22
JooDdae <kyo20111@naver.com>
2019-07-01 21:30:44 +0900

    upload generator

---
+++ draft/FUNCTION/generator.cpp
@@ -0,0 +1,27 @@
+#include "testlib.h"
+
+using namespace std;
+
+int main(int argc, char* argv[]){
+	registerGen(argc, argv, 1);
+
+	int a = atoi(argv[1]);
+
+	int N = rnd.next(2, a);
+	int in[N+1];
+	for(int i=1;i<=N;i++) in[i] = i;
+	printf("%d\n",N);
+	for(int i=1;i<=N;i++){
+        shuffle(in+1,in+1+N);
+        int cnt = rnd.next(0, N-2);
+        for(int j=1;j<=cnt;j++){
+            if(in[j] == 1 || in[j] == i){
+                cnt++;
+                continue;
+            }
+            printf("%d ",in[j]);
+        }
+        printf("-1\n");
+	}
+	return 0;
+}
