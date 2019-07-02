0ec7029e928ee1c5e5e530f575c92a3253d3304d
JooDdae <kyo20111@naver.com>
2019-07-02 14:05:22 +0900

    update solution

--- draft/FUNCTION/solution.cpp
+++ draft/FUNCTION/solution.cpp
@@ -1,43 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-int n,chk[1010],fl;
+int n,chk[1010],visited[1010],fl;
 vector<int> v[1010];
 priority_queue<int,vector<int>,greater<int>> pq;
-bool dfs(int a){
-    if(chk[a]){
-        fl = a;
-        pq.push(a);
-        return true;
-    }
-    chk[a] = 1;
-    for(int nxt : v[a]){
-        if(dfs(nxt)){
-            if(fl){
-                if(fl == a) fl=0;
-                else pq.push(a);
-            }
-            return true;
-        }
-    }
-    chk[a] = 0;
-    return false;
+bool f(int cur){
+	if(visited[cur]) return false;
+	if(chk[cur]){
+		fl = cur;
+		return true;
+	}
+	chk[cur] = 1;
+	for(int nxt : v[cur]){
+		if(f(nxt)){
+			if(fl){
+				pq.push(cur);
+				if(cur == fl) fl = 0;
+			}
+			return true;
+		}
+	}
+	chk[cur] = 0;
+	visited[cur] = 1;
+	return false;
 }
-int main(){
-    scanf("%d",&n);
-    for(int i=1;i<=n;i++){
-        while(1){
-            int a; scanf("%d",&a);
-            if(a == -1) break;
-            v[i].push_back(a);
-        }
-    }
+int main() {
+	scanf("%d",&n);
+	for(int i=1;i<=n;i++){
+		while(1){
+			int a; scanf("%d",&a);
+			if(a == -1) break;
+			v[i].push_back(a);
+		}
+	}
 
-    if(dfs(1)){
-        printf("YES\n");
-        while(!pq.empty()){
-            printf("%d ",pq.top());
-            pq.pop();
-        }
-    }else printf("NO");
+	if(f(1)){
+		printf("YES\n");
+		while(!pq.empty()){
+			printf("%d ",pq.top());
+			pq.pop();
+		}
+	}else printf("NO");
 }
